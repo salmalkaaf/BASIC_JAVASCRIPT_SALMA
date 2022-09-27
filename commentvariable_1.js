0084ea7c8d51790aeb66fb33c2802463df86d054
salmalkaaf <sal.alkaaf@gmail.com>
2022-09-27 13:27:44 +0700

    2022-09-27_javascript_salma

---
+++ commentvariable_1.js
@@ -0,0 +1,41 @@
+//single line comment 
+/*
+multiple line comments
+first chapter is addition
+*/
+
+console.log('first: pertambahan')
+var x = 23.5;
+var y =24.5;
+var addition = x+y;
+console.log(addition)
+
+/*
+multiple line comments
+second chapter is substraction
+*/
+console.log('second: pengurangan')
+var x = 30;
+var y =26;
+var addition = x-y;
+console.log(addition)
+
+/*
+multiple line comments
+third chapter is division
+*/
+console.log('third: pembagian')
+var x = 50;
+var y =5;
+var addition = x/y;
+console.log(addition)
+
+/*
+multiple line comments
+fourth chapter is multiplication
+*/
+console.log('fourth: perkalian')
+var x = 5;
+var y =2.5;
+var addition = x*y;
+console.log(addition)
---
+++ mathOperator_4_exercise.js
@@ -0,0 +1,25 @@
+console.log('===increment===')
+var j = 0
+var k = 10
+var j = k++
+console.log(j)
+console.log(k + 1)
+
+var i = 0
+var h = 10
+var i = ++h
+console.log(i + 1)
+console.log(h + 1)
+
+console.log('===decrement===')
+var l = 0
+var m = 10
+var l = m--
+console.log(l)
+console.log(m - 1)
+
+var o = 0
+var p = 10
+var o = --p
+console.log(o - 1)
+console.log(p - 1)
